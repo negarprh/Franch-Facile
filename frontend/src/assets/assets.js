aa13ac130815f233fb581752b9450ede9b7a4ccd
Tara niaki <taraniaki@gmail.com>
2025-01-14 20:13:02 -0500

    card1 images added

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,11 +2,13 @@ import logo from "./logo.png";
 import cap from "./cap.png";
 import book from "./book.png";
 import bgwlc from "./welcombg.png";
+import grammericon from "./grammer-icon.png";
 
 export const assets = {
     logo,
     cap,
     book,
-    bgwlc
+    bgwlc,
+    grammericon
 
 }
