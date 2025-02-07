d9ff1260bcc314f8ea0d6e8fde71640ea2ba7324
Tara niaki <taraniaki@gmail.com>
2025-02-07 14:46:39 -0500

    image updated

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -3,12 +3,14 @@ import cap from "./cap.png";
 import book from "./book.png";
 import bgwlc from "./welcombg.png";
 import grammericon from "./grammer-icon.png";
+import contact from "./contact.JPG"
 
 export const assets = {
     logo,
     cap,
     book,
     bgwlc,
-    grammericon
+    grammericon,
+    contact
 
 }
